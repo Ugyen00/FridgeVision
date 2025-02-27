a28d1b168b545249e76bd76517733624a7f0f772
Ugyen Dendup <61900463+Ugyen00@users.noreply.github.com>
2025-02-27 23:00:51 +0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         // Fetch low-stock items from the API
         const fetchOutOfStockItems = async () => {
             try {
-                const response = await fetch('https://fridgevision-backend.onrender.com/api/real-time-items');
+                const response = await fetch('https://fridgevision-backend-u8pk.onrender.com/api/real-time-items');
                 if (!response.ok) {
                     throw new Error('Failed to fetch data');
                 }
